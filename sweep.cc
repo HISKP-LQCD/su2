0779ea965dea23696e009524d464b89815bc943b
Carsten Urbach <curbach@gmx.de>
2017-03-30 11:49:03 +0200

    line breaks added, rU renamed to R

--- sweep.cc
+++ sweep.cc
@@ -5,12 +5,13 @@
 #include<random>
 #include<vector>
 
-double sweep(gaugeconfig &U, const size_t seed, const double delta, const size_t N_hit, const double beta) {
+double sweep(gaugeconfig &U, const size_t seed, const double delta, 
+             const size_t N_hit, const double beta) {
   std::mt19937 engine(seed);
   std::uniform_real_distribution<double> uniform(0., 1.);
 
   size_t rate = 0;
-  su2 rU(0., 0.);
+  su2 R(0., 0.);
   std::vector<size_t> x = {0, 0, 0, 0};
   for(x[0] = 0; x[0] < U.getLt(); x[0]++) {
     for(x[1] = 0; x[1] < U.getLs(); x[1]++) {
@@ -19,13 +20,13 @@ double sweep(gaugeconfig &U, const size_t seed, const double delta, const size_t
           for(size_t mu = 0; mu < 4; mu++) {
             su2 K = get_staples(U, x, mu);
             for(size_t n = 0; n < N_hit; n++) {
-              bool accept = false;
-              random_su2(rU, engine, delta);
-              double deltaS = beta/static_cast<double>(N_c)*(trace<su2>(U(x, mu) * K) - trace(U(x, mu) * rU * K));
-              if(deltaS < 0) accept = true;
-              else accept = (uniform(engine) < exp(-deltaS));
+              random_su2(R, engine, delta);
+              double deltaS = beta/static_cast<double>(N_c)*
+                ((U(x, mu) * K).trace() - (U(x, mu) * R * K).trace());
+              bool accept = (deltaS < 0);
+              if(!accept) accept = (uniform(engine) < exp(-deltaS));
               if(accept) {
-                U(x, mu) = U(x, mu) * rU;
+                U(x, mu) = U(x, mu) * R;
                 U(x, mu).rescale();
                 rate += 1;
               }

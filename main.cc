62c45f0ceeade2cfb738244cfac54b80253a98d3
Carsten Urbach <curbach@gmx.de>
2017-07-10 11:50:07 +0200

    added gradient flow as measurement

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include"random_gauge_trafo.hh"
 #include"sweep.hh"
 #include"wilsonloop.hh"
+#include"gradient_flow.hh"
 
 #include<iostream>
 #include<vector>
@@ -21,7 +22,7 @@ int main() {
   const double delta = 0.1;
   const size_t N_save = 20;
   gaugeconfig U(Ls, Lt, beta);
-  U = hotstart(Ls, Lt, 123456, 0.2);
+  U = hotstart(Ls, Lt, 123456, 0.1);
   //U = coldstart(Ls, Lt);
   
   double plaquette = gauge_energy(U);
@@ -36,10 +37,16 @@ int main() {
     rate += sweep(U, 13243546, delta, N_hit, beta);
     cout << i << " " << gauge_energy(U)/U.getVolume()/N_c/6. << endl;
     if(i > 0 && i % N_save == 0) {
-      std::ostringstream os;
-      os << "wilsonloop." << i << ".dat" << std::ends;
-      std::string filename = os.str();
-      compute_all_loops(U, filename);
+      {
+        std::ostringstream os;
+        os << "wilsonloop." << i << ".dat" << std::ends;
+        compute_all_loops(U, os.str());
+      }
+      {
+        std::ostringstream os;
+        os << "gradient_flow." << i << ".dat" << std::ends;
+        gradient_flow(U, os.str());
+      }
     }
   }
   cout << rate/static_cast<double>(N_meas) << endl;

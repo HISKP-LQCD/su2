02518d180b1fe03ecace4333761ba95b842977ea
Carsten Urbach <curbach@gmx.de>
2017-07-20 14:50:58 +0200

    random engine started in main

--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include<iostream>
 #include<sstream>
 #include<vector>
+#include<random>
 
 using std::vector;
 using std::cout;
@@ -16,16 +17,18 @@ using std::endl;
 
 
 int main() {
-  const size_t Ls = 8, Lt = 8;
+  const size_t Ls = 8, Lt = 16;
   const double beta = 2.3;
   const size_t N_hit = 10;
   const size_t N_meas = 2000;
   const double delta = 0.1;
   const size_t N_save = 20;
+  const int seed = 13526463;
   gaugeconfig U(Ls, Lt, beta);
   U = hotstart(Ls, Lt, 123456, 0.1);
   //U = coldstart(Ls, Lt);
   
+
   double plaquette = gauge_energy(U);
   cout << "Initital Plaquette: " << plaquette/U.getVolume()/N_c/6. << endl; 
 
@@ -35,7 +38,8 @@ int main() {
 
   double rate = 0.;
   for(size_t i = 0; i < N_meas; i++) {
-    rate += sweep(U, 13243546+i, delta, N_hit, beta);
+    std::mt19937 engine(seed+i);
+    rate += sweep(U, engine, delta, N_hit, beta);
     cout << i << " " << gauge_energy(U)/U.getVolume()/N_c/6. << endl;
     if(i > 0 && (i % N_save) == 0) {
       std::ostringstream os;
--- sweep.hh
+++ sweep.hh
@@ -1,5 +1,42 @@
 #pragma once
 
 #include"gaugeconfig.hh"
+#include"gaugeconfig.hh"
+#include"random_su2.hh"
+#include"get_staples.hh"
+#include<random>
+#include<vector>
 
-double sweep(gaugeconfig &U, const size_t seed, const double delta, const size_t N_hit, const double beta);
+template<class URNG> double sweep(gaugeconfig &U, URNG &engine, const double delta, 
+                                  const size_t N_hit, const double beta) {
+
+  std::uniform_real_distribution<double> uniform(0., 1.);
+
+  size_t rate = 0;
+  su2 R(0., 0.);
+  std::vector<size_t> x = {0, 0, 0, 0};
+  for(x[0] = 0; x[0] < U.getLt(); x[0]++) {
+    for(x[1] = 0; x[1] < U.getLs(); x[1]++) {
+      for(x[2] = 0; x[2] < U.getLs(); x[2]++) {
+        for(x[3] = 0; x[3] < U.getLs(); x[3]++) {
+          for(size_t mu = 0; mu < 4; mu++) {
+            su2 K = get_staples(U, x, mu);
+            for(size_t n = 0; n < N_hit; n++) {
+              random_su2(R, engine, delta);
+              double deltaS = beta/static_cast<double>(N_c)*
+                (trace(U(x, mu) * K) - trace(U(x, mu) * R * K));
+              bool accept = (deltaS < 0);
+              if(!accept) accept = (uniform(engine) < exp(-deltaS));
+              if(accept) {
+                U(x, mu) = U(x, mu) * R;
+                U(x, mu).rescale();
+                rate += 1;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return( double(rate)/double(N_hit)/double(U.getSize()));
+}

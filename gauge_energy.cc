7a854b7d266151f64615ceedbcfde5b91e05fdba
Carsten Urbach <curbach@gmx.de>
2017-07-26 12:40:28 +0200

    added openMP to gauge_energy computation

--- gauge_energy.cc
+++ gauge_energy.cc
@@ -1,6 +1,9 @@
 #include"gaugeconfig.hh"
 #include"gauge_energy.hh"
 #include"su2.hh"
+#ifdef _USE_OMP_
+#  include<omp.h>
+#endif
 
 // this is the Wilson plaquette gauge energy
 // \sum_mu \sum_nu<mu tr(P_{mu nu})
@@ -8,28 +11,48 @@
 // checked for gauge invariance
 
 double gauge_energy(gaugeconfig &U) {
-  double res = 0.;
 
-  std::vector<size_t> x = {0, 0, 0, 0};
-  for(x[0] = 0; x[0] < U.getLt(); x[0]++) {
-    for(x[1] = 0; x[1] < U.getLs(); x[1]++) {
-      for(x[2] = 0; x[2] < U.getLs(); x[2]++) {
-        for(x[3] = 0; x[3] < U.getLs(); x[3]++) {
-          std::vector<size_t> xplusmu = x;
-          std::vector<size_t> xplusnu = x;
-          for(size_t mu = 0; mu < 3; mu++) {
-            for(size_t nu = mu+1; nu < 4; nu++) {
-              xplusmu[mu] += 1;
-              xplusnu[nu] += 1;
-              res += trace(U(x, mu) * U(xplusmu, nu) *
-                           U(xplusnu, mu).dagger()*U(x, nu).dagger());
-              xplusmu[mu] -= 1;
-              xplusnu[nu] -= 1;
+  double res = 0.;
+#ifdef _USE_OMP_
+  int threads = omp_get_max_threads();
+  static double * omp_acc = new double[threads];
+#pragma omp parallel
+  {
+    int thread_num = omp_get_thread_num();
+#endif
+    double tmp = 0.;
+ 
+#pragma omp for
+    for(size_t x0 = 0; x0 < U.getLt(); x0++) {
+      for(size_t x1 = 0; x1 < U.getLs(); x1++) {
+        for(size_t x2 = 0; x2 < U.getLs(); x2++) {
+          for(size_t x3 = 0; x3 < U.getLs(); x3++) {
+            std::vector<size_t> x = {x0, x1, x2, x3};
+            std::vector<size_t> xplusmu = x;
+            std::vector<size_t> xplusnu = x;
+            for(size_t mu = 0; mu < 3; mu++) {
+              for(size_t nu = mu+1; nu < 4; nu++) {
+                xplusmu[mu] += 1;
+                xplusnu[nu] += 1;
+                tmp += trace(U(x, mu) * U(xplusmu, nu) *
+                             U(xplusnu, mu).dagger()*U(x, nu).dagger());
+                xplusmu[mu] -= 1;
+                xplusnu[nu] -= 1;
+              }
             }
           }
         }
       }
     }
+#ifdef _USE_OMP_
+    omp_acc[thread_num] = tmp;
+    res = 0.;
   }
-  return(res);
+  for(size_t i = 0; i < threads; i++) {
+    res += omp_acc[i];
+  }
+#else
+  res = tmp;
+#endif
+  return res;
 }

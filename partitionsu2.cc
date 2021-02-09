41d8a3aaf83ed075f77d3988ab9459c30f11b47e
Carsten Urbach <curbach@gmx.de>
2021-02-09 14:53:46 +0100

    a trial merge

---
+++ partitionsu2.cc
@@ -0,0 +1,74 @@
+#include"su2.hh"
+#include"partitionsu2.hh"
+#include<complex>
+#include<math.h>
+
+su2 operator*(const Gsu2 &U1, const Gsu2 &U2) {
+  unsigned int m = U1.m;
+  Complex a1(U1.s[0]*sqrt(static_cast<double>(U1.j[0])/static_cast<double>(m)),
+             U1.s[1]*sqrt(static_cast<double>(U1.j[1])/static_cast<double>(m)));
+  Complex a2(U2.s[0]*sqrt(static_cast<double>(U2.j[0])/static_cast<double>(m)),
+             U2.s[1]*sqrt(static_cast<double>(U2.j[1])/static_cast<double>(m)));
+  Complex b1(U1.s[2]*sqrt(static_cast<double>(U1.j[2])/static_cast<double>(m)),
+             U1.s[3]*sqrt(static_cast<double>(U1.j[3])/static_cast<double>(m)));
+  Complex b2(U2.s[2]*sqrt(static_cast<double>(U2.j[2])/static_cast<double>(m)),
+             U2.s[3]*sqrt(static_cast<double>(U2.j[3])/static_cast<double>(m)));
+  su2 res(a1*a2 - b1*std::conj(b2), a1*b2 + b1*std::conj(a2));
+  return(res);
+}
+
+su2 operator*(const su2 &U1, const Gsu2 &U2) {
+  unsigned int m = U2.m;
+  Complex a2(U2.s[0]*sqrt(static_cast<double>(U2.j[0])/static_cast<double>(m)),
+             U2.s[1]*sqrt(static_cast<double>(U2.j[1])/static_cast<double>(m)));
+  Complex b2(U2.s[2]*sqrt(static_cast<double>(U2.j[2])/static_cast<double>(m)),
+             U2.s[3]*sqrt(static_cast<double>(U2.j[3])/static_cast<double>(m)));
+  su2 res(U1.geta()*a2 - U1.getb()*std::conj(b2), U1.geta()*b2 + U1.getb()*std::conj(a2));
+  return(res);
+}
+
+su2 operator*(const Gsu2 &U1, const su2 &U2) {
+  unsigned int m = U1.m;
+  Complex a1(U1.s[0]*sqrt(static_cast<double>(U1.j[0])/static_cast<double>(m)),
+             U1.s[1]*sqrt(static_cast<double>(U1.j[1])/static_cast<double>(m)));
+  Complex b1(U1.s[2]*sqrt(static_cast<double>(U1.j[2])/static_cast<double>(m)),
+             U1.s[3]*sqrt(static_cast<double>(U1.j[3])/static_cast<double>(m)));
+  su2 res(a1*U2.geta() - b1*std::conj(U2.getb()),  a1*U2.getb() + b1*std::conj(U2.geta()));
+  return(res);
+}
+
+su2 operator*(const Osu2 &U1, const Osu2 &U2) {
+  const double J1 = U1.getJ(), J2 = U2.getJ();
+  Complex a1(U1.s[0]*static_cast<double>(U1.j[0])/J1,
+             U1.s[1]*static_cast<double>(U1.j[1])/J1);
+  Complex a2(U2.s[0]*static_cast<double>(U2.j[0])/J2,
+             U2.s[1]*static_cast<double>(U2.j[1])/J2);
+  Complex b1(U1.s[2]*static_cast<double>(U1.j[2])/J1,
+             U1.s[3]*static_cast<double>(U1.j[3])/J1);
+  Complex b2(U2.s[2]*static_cast<double>(U2.j[2])/J2,
+             U2.s[3]*static_cast<double>(U2.j[3])/J2);
+  su2 res(a1*a2 - b1*std::conj(b2), a1*b2 + b1*std::conj(a2));
+  return(res);
+}
+
+su2 operator*(const su2 &U1, const Osu2 &U2) {
+  const double J2 = U2.getJ();
+  Complex a2(U2.s[0]*static_cast<double>(U2.j[0])/J2,
+             U2.s[1]*static_cast<double>(U2.j[1])/J2);
+  Complex b2(U2.s[2]*static_cast<double>(U2.j[2])/J2,
+             U2.s[3]*static_cast<double>(U2.j[3])/J2);
+  su2 res(U1.geta()*a2 - U1.getb()*std::conj(b2), U1.geta()*b2 + U1.getb()*std::conj(a2));
+  return(res);
+}
+
+su2 operator*(const Osu2 &U1, const su2 &U2) {
+  const double J1 = U1.getJ();
+  Complex a1(U1.s[0]*static_cast<double>(U1.j[0])/J1,
+             U1.s[1]*static_cast<double>(U1.j[1])/J1);
+  Complex b1(U1.s[2]*static_cast<double>(U1.j[2])/J1,
+             U1.s[3]*static_cast<double>(U1.j[3])/J1);
+  su2 res(a1*U2.geta() - b1*std::conj(U2.getb()),  a1*U2.getb() + b1*std::conj(U2.geta()));
+  return(res);
+}
+
+
---
+++ partitionsu2.hh
@@ -0,0 +1,167 @@
+#pragma once
+
+#include<complex>
+#include<math.h>
+#include<cmath>
+#include<cassert>
+#include"su2.hh"
+
+using Complex = std::complex<double>;
+
+class _partitionsu2 {
+public:
+  explicit _partitionsu2() : m(0) {
+    j[0] = m;
+    j[1] = 0;
+    j[2] = 0;
+    j[3] = 0;
+    s[0] = +1;
+    s[1] = +1;
+    s[2] = +1;
+    s[3] = +1;
+  }
+  explicit _partitionsu2(size_t m) : m(m) {
+    j[0] = m;
+    j[1] = 0;
+    j[2] = 0;
+    j[3] = 0;
+    s[0] = +1;
+    s[1] = +1;
+    s[2] = +1;
+    s[3] = +1;
+    assert((m == j[0] + j[1] + j[2] + j[3]));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+  }
+  explicit _partitionsu2(size_t _m,
+                 size_t * _j,
+                 int * _s){
+    for(int i = 0; i < 4; i++) {
+      j[i] = _j[i];
+      s[i] = _s[i];
+    }
+    m=_m;
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+  }
+  explicit _partitionsu2(size_t m,
+                 size_t j1, size_t j2, size_t j3,
+                 int s1=1, int s2=1, int s3=1, int s4=1) : m(m) {
+    j[0] = j1;
+    j[1] = j2;
+    j[2] = j3;
+    s[0] = s1;
+    s[1] = s2;
+    s[2] = s3;
+    s[3] = s4;
+    j[3] = m - j[0] - j[1] - j[2];
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+  }
+  friend su2 operator*(const _partitionsu2 &U1, const _partitionsu2 &U2);
+  friend su2 operator*(const su2 &U1, const _partitionsu2 &U2);
+  friend su2 operator*(const _partitionsu2 &U1, const su2 &U2);
+
+  size_t getj(const size_t i) const {
+    return(j[i]);
+  }
+  void setjpair(const size_t i1, const size_t i2,
+               const size_t _j1, const size_t _j2) {
+    j[i1] = _j1;
+    j[i2] = _j2;
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    return;
+  }
+  int gets(const size_t i) const {
+    return(s[i]);
+  }
+  void sets(const size_t i, const int _s) {
+    s[i] = _s;
+    return;
+  }
+  size_t getm() const {
+    return(m);
+  }
+  void operator=(const _partitionsu2 &U) {
+    for(int i = 0; i < 4; i++) {
+      j[i] = U.getj(i);
+      s[i] = U.gets(i);
+      m = U.getm();
+    }
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+  }
+  void set(const size_t * const _j, const int * const _s, const size_t _m) {
+    for(int i = 0; i < 4; i++) {
+      j[i] = _j[i];
+      s[i] = _s[i];
+    }
+    m=_m;
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+  }
+  _partitionsu2 dagger() const {
+    return(_partitionsu2(m, j[0], j[1], j[2], s[0], -s[1], -s[2], -s[3]));
+  }
+  double trace() {
+    return(2 * s[0] * sqrt(static_cast<double>(j[0])/static_cast<double>(m)));
+  }
+  Complex det() {
+    return((static_cast<double>(j[0]) + static_cast<double>(j[1]) +
+            static_cast<double>(j[2]) + static_cast<double>(j[3]))/static_cast<double>(m)); 
+  }
+  void restoreSU() {
+    assert((m == (j[0] + j[1] + j[2] + j[3])));
+    assert(((abs(s[0]) == 1) && (abs(s[1]) == 1) && (abs(s[2]) == 1) && (abs(s[3]) == 1)));
+    return;
+  }
+
+protected:
+  size_t m;
+  size_t j[4];
+  int s[4];
+};
+
+class _Gsu2 : private _partitionsu2 {
+public:
+  double trace() {
+    return(2 * s[0] * sqrt(static_cast<double>(j[0])/static_cast<double>(m)));
+  }
+  Complex det() {
+    return((static_cast<double>(j[0]) + static_cast<double>(j[1]) +
+            static_cast<double>(j[2]) + static_cast<double>(j[3]))/static_cast<double>(m)); 
+  }
+  friend su2 operator*(const _Gsu2 &U1, const _Gsu2 &U2);
+  friend su2 operator*(const su2 &U1, const _Gsu2 &U2);
+  friend su2 operator*(const _Gsu2 &U1, const su2 &U2);
+};
+
+class _Osu2 : private _partitionsu2 {
+public:
+  double getJ() const {
+    return(sqrt(static_cast<double>(j[0]*j[0] + j[1]*j[1] + j[2]*j[2] + j[3]*j[3])));
+  }
+  double trace() {
+    return(2 * s[0] * static_cast<double>(j[0])/getJ());
+  }
+  Complex det() {
+    return((static_cast<double>(j[0])*static_cast<double>(j[0]) +
+            static_cast<double>(j[1])*static_cast<double>(j[1]) +
+            static_cast<double>(j[2])*static_cast<double>(j[2]) +
+            static_cast<double>(j[3])*static_cast<double>(j[3])) /
+           static_cast<double>(getJ()*getJ())); 
+  }
+  friend su2 operator*(const _Osu2 &U1, const _Osu2 &U2);
+  friend su2 operator*(const su2 &U1, const _Osu2 &U2);
+  friend su2 operator*(const _Osu2 &U1, const su2 &U2);
+};
+
+su2 operator*(const _Gsu2 &U1, const _Gsu2 &U2);
+su2 operator*(const su2 &U1, const _Gsu2 &U2);
+su2 operator*(const _Gsu2 &U1, const su2 &U2);
+
+su2 operator*(const _Osu2 &U1, const _Osu2 &U2);
+su2 operator*(const su2 &U1, const _Osu2 &U2);
+su2 operator*(const _Osu2 &U1, const su2 &U2);
+
+using Gsu2 = _Gsu2;
+using Osu2 = _Osu2;

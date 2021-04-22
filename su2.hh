b0613465dec3f7cfb3cd540dc09a7d04fc0b10ab
Carsten Urbach <curbach@gmx.de>
2021-04-22 10:45:24 +0200

    get rid of su2.cc

--- su2.hh
+++ su2.hh
@@ -15,9 +15,9 @@ public:
   explicit _su2(Complex a, Complex b) : a(a), b(b) {}
   _su2(const _su2& U) : a(U.a), b(U.b) {}
 
-  friend _su2 operator+(const _su2 &U1, const _su2 &U2);
-  friend _su2 operator-(const _su2 &U1, const _su2 &U2);
-  friend _su2 operator*(const _su2 &U1, const _su2 &U2);
+  friend inline _su2 operator+(const _su2 &U1, const _su2 &U2);
+  friend inline _su2 operator-(const _su2 &U1, const _su2 &U2);
+  friend inline _su2 operator*(const _su2 &U1, const _su2 &U2);
   _su2& operator*=(const _su2 &U1) {
     Complex a = this->a;
     this->a = a*U1.a - this->b*std::conj(U1.b);
@@ -30,17 +30,17 @@ public:
                 Complex(std::round(std::real(b)*dn), std::round(std::imag(b)*dn))/dn);
   }
 
-  Complex geta() const {
+  inline Complex geta() const {
     return(a);
   }
-  Complex getb() const {
+  inline Complex getb() const {
     return(b);
   }
-  void operator=(const _su2 &U) {
+  inline void operator=(const _su2 &U) {
     a = U.geta();
     b = U.getb();
   }
-  void operator+=(const _su2 &U) {
+  inline void operator+=(const _su2 &U) {
     a += U.a;
     b += U.b;
   }
@@ -48,10 +48,10 @@ public:
     a = _a;
     b = _b;
   }
-  _su2 dagger() const {
+  inline _su2 dagger() const {
     return(_su2(std::conj(a), -b));
   }
-  double retrace() {
+  inline double retrace() {
     return(2.*std::real(a));
   }
   Complex det() {
@@ -82,9 +82,27 @@ template<> inline _su2 traceless_antiherm(const _su2& x) {
   return(_su2(0.5*(x.geta()-std::conj(x.geta())), x.getb()));
 }
 
-_su2 operator*(const _su2 &U1, const _su2 &U2);
-_su2 operator+(const _su2 &U1, const _su2 &U2);
-_su2 operator-(const _su2 &U1, const _su2 &U2);
+inline _su2 operator*(const _su2 &U1, const _su2 &U2) {
+  _su2 res;
+  res.a = U1.a*U2.a - U1.b*std::conj(U2.b);
+  res.b = U1.a*U2.b + U1.b*std::conj(U2.a);
+  return(res);
+}
+
+inline _su2 operator+(const _su2 &U1, const _su2 &U2) {
+  _su2 res;
+  res.a = U1.a + U2.a;
+  res.b = U1.b + U2.b;
+  return(res);
+}
+
+inline _su2 operator-(const _su2 &U1, const _su2 &U2) {
+  _su2 res;
+  res.a = U1.a - U2.a;
+  res.b = U1.b - U2.b;
+  return(res);
+}
+
 
 using su2 = _su2;
 

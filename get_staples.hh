1d5917037aa9490e6da61bc08543a9499676c41a
Christiane Gross <chgross.wickede@gmail.com>
2021-11-08 11:54:40 +0100

    added anisotropy: added function get_staples_anisotrope: for each staple, the weight deltatau or 1/deltatau is included in the sum, with the function scalarmultiplyadd. Not checked for SU2, but compiler does not throw errors. Changed sweep, main-u1 to take anisotropy into account, default delta tau=1=isotrope.

--- get_staples.hh
+++ get_staples.hh
@@ -30,3 +30,43 @@ template<class T, class S> void get_staples(T &K, gaugeconfig<S> &U,
   }
 }
 
+//stores c1*U1+complex(c2*U2) in U1
+//variable K used for accumulation is of type Complex for u1 (and su2 for su2?)
+inline void scalarmultiplyadd(Complex &U1, _u1 U2, double c1, double c2) {
+    U1=Complex(c1,0.)*U1+Complex(c2,0.)*Complex(U2);
+}
+
+//stores c1*U1+c2*U2 in U1
+void scalarmultiplyadd(_su2 &U1, _su2 U2, double c1, double c2) {
+    _su2 copyU1, copyU2;
+    copyU1.set(c1*U1.geta(), c1*U1.getb());
+    copyU2.set(c2*U2.geta(), c2*U2.getb());
+    U1=copyU1+copyU2;
+}
+
+template<class T, class S> void get_staples_anisotrope(T &K, gaugeconfig<S> &U,
+                                   vector<size_t> const x,
+                                   const size_t mu, const double deltatau) {
+  vector<size_t> x1 = x, x2 = x;
+  double factor=0;
+  x1[mu] += 1;
+  for(size_t nu = 0; nu < U.getndims(); nu++) {
+    if(nu != mu) {
+      factor=((nu==0)||(mu==0)?1.0/deltatau:deltatau);
+      x2[nu]++;
+      scalarmultiplyadd(K, U(x1, nu) * U(x2, mu).dagger() * U(x, nu).dagger(), 1.0, factor);
+      x2[nu]--;
+    }
+  }
+  for(size_t nu = 0; nu < U.getndims(); nu++) {
+    if(nu != mu) {
+      factor=((nu==0)||(mu==0)?1.0/deltatau:deltatau);
+      x1[nu]--;
+      x2[nu]--;
+      scalarmultiplyadd(K, U(x1, nu).dagger() * U(x2, mu).dagger() * U(x2, nu), 1.0, factor);
+      x2[nu]++;
+      x1[nu]++;
+    }
+  }
+}
+
--- main-u1.cc
+++ main-u1.cc
@@ -26,6 +26,7 @@ int main(int ac, char* av[]) {
 
   size_t N_hit = 10;
   double delta = 0.1;
+  double deltatau = 1.0;
 
   cout << "## Metropolis Algorithm for U(1) gauge theory" << endl;
   cout << "## (C) Carsten Urbach <urbach@hiskp.uni-bonn.de> (2017, 2021)" << endl;
@@ -38,6 +39,7 @@ int main(int ac, char* av[]) {
   desc.add_options()
     ("nhit", po::value<size_t>(&N_hit)->default_value(10), "N_hit")
     ("delta,d", po::value<double>(&delta), "delta")
+    ("tau", po::value<double>(&deltatau)->default_value(1.0), "deltatau, characteristic of anisotropy")
     ;
 
   int err = parse_commandline(ac, av, desc, gparams);
@@ -59,11 +61,11 @@ int main(int ac, char* av[]) {
   double plaquette = gauge_energy(U);
   double fac = 2./U.getndims()/(U.getndims()-1);
   const double normalisation = fac/U.getVolume();
-  cout << "Initital Plaquette: " << plaquette*normalisation << endl; 
+  cout << "## Initital Plaquette: " << plaquette*normalisation << endl; 
 
   random_gauge_trafo(U, 654321);
   plaquette = gauge_energy(U);
-  cout << "Plaquette after rnd trafo: " << plaquette*normalisation << endl; 
+  cout << "## Plaquette after rnd trafo: " << plaquette*normalisation << endl; 
 
 
   std::ofstream os;
@@ -74,13 +76,13 @@ int main(int ac, char* av[]) {
   double rate = 0.;
   for(size_t i = gparams.icounter; i < gparams.N_meas + gparams.icounter; i++) {
     std::mt19937 engine(gparams.seed+i);
-    rate += sweep(U, engine, delta, N_hit, gparams.beta);
+    rate += sweep(U, engine, delta, N_hit, gparams.beta, deltatau);
     double energy = gauge_energy(U);
     double E = 0., Q = 0.;
-    energy_density(U, E, Q);
+    //~ energy_density(U, E, Q);
     cout << i << " " << std::scientific << std::setw(18) << std::setprecision(15) << energy*normalisation << " " << Q << " ";
     os << i << " " << std::scientific << std::setw(18) << std::setprecision(15) << energy*normalisation << " " << Q << " ";
-    energy_density(U, E, Q, false);
+    //~ energy_density(U, E, Q, false);
     cout << Q << endl;
     os << Q << endl;
     if(i > 0 && (i % gparams.N_save) == 0) {

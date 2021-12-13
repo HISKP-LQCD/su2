243e9b763fa07b3e5aa09ec503ca21a4294316f2
Christiane Gross <chgross.wickede@gmail.com>
2021-12-13 15:07:50 +0100

    minor corrections in potential, especially clearer filenames, in apesmearing now also possible to smear only spaial links

--- potentialu1.cc
+++ potentialu1.cc
@@ -25,10 +25,10 @@ using std::endl;
 int main(int ac, char* av[]) {
   general_params gparams;
   size_t nstep;
-  size_t n_steps;
   bool append;
   size_t n_apesmear;
   double alpha;
+  bool smearspacial;
 
   cout << "## Measuring Tool for U(1) gauge theory" << endl;
   cout << "## (C) Carsten Urbach <urbach@hiskp.uni-bonn.de> (2017)" << endl;
@@ -40,9 +40,9 @@ int main(int ac, char* av[]) {
   desc.add_options()
     ("append", po::value<bool>(&append)->default_value(false), "are measurements appended to an existing file, or should it be overwritten?")
     ("nstep", po::value<size_t>(&nstep)->default_value(1), "measure each nstep config")
-    ("nsteps", po::value<size_t>(&n_steps)->default_value(1000), "n_steps")
     ("napesmears", po::value<size_t>(&n_apesmear)->default_value(0), "number of APE smearings done on the lattice before measurement")
     ("apealpha", po::value<double>(&alpha)->default_value(1.0), "parameter alpha for APE smearings")
+    ("spacialsmear", po::value<bool>(&smearspacial)->default_value(false), "should smearing be done only for spacial links?")
     ;
 
   int err = parse_commandline(ac, av, desc, gparams);
@@ -59,7 +59,7 @@ int main(int ac, char* av[]) {
   //~ print heads of columns: W(r, t), W(x, y)
   if(!append){
   for (size_t x=1 ; x<=gparams.Lx/2 ; x++){
-    sprintf(filename, "result.u1potential.b%f.xi%f.nape%lu.alpha%f.x%lu",U.getBeta(), gparams.xi, n_apesmear, alpha, x);
+    sprintf(filename, "result.u1potential.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%f.x%lu",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha, x);
     resultfile.open(filename, std::ios::out);
     resultfile << "#";
     for (size_t t=1 ; t<=gparams.Lt/2 ; t++){
@@ -82,20 +82,21 @@ int main(int ac, char* av[]) {
     U.load(os.str());
     //smear lattice
     for (size_t smears=0 ; smears<n_apesmear ; smears +=1){
-      smearlatticeape(U, alpha);
+      smearlatticeape(U, alpha, smearspacial);
     }
     //~ //calculate wilsonloops
     for (size_t x=1 ; x<=gparams.Lx/2 ; x++){
-      sprintf(filename, "result.u1potential.b%f.xi%f.nape%lu.alpha%f.x%lu",U.getBeta(), gparams.xi, n_apesmear, alpha, x);
+      sprintf(filename, "result.u1potential.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%f.x%lu",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha, x);
       resultfile.open(filename, std::ios::app);
     
     //Measure for two radii each time by changing one of the coordinates not needed for the measurement
     //Measure (x,t) and (x,y), with "t" the anisotropic direction, "x" the "first" isotropic direction and "y" taken as the average of the other two directions
       for (size_t t=1 ; t<=gparams.Lt/2 ; t++){
         loop=wilsonloop_non_planar(U, {t, x, 0, 0});
-        loop=wilsonloop_non_planar(U, {t, 0, x, 0});
-        loop=wilsonloop_non_planar(U, {t, 0, 0, x});
+        loop+=wilsonloop_non_planar(U, {t, 0, x, 0});
+        loop+=wilsonloop_non_planar(U, {t, 0, 0, x});
         resultfile << std::setw(14) << std::fixed << loop/U.getVolume()/3.0 << "  " ; 
+        
         loop= wilsonloop_non_planar(U, {t, x, 1, 0});
         loop+=wilsonloop_non_planar(U, {t, x, 0, 1});
         loop+=wilsonloop_non_planar(U, {t, 1, x, 0});
@@ -103,7 +104,7 @@ int main(int ac, char* av[]) {
         loop+=wilsonloop_non_planar(U, {t, 0, 1, x});
         loop+=wilsonloop_non_planar(U, {t, 1, 0, x});
         resultfile << std::setw(14) << std::fixed << loop/U.getVolume()/6.0 << "  " ;
-      }   
+      }
       for (size_t y=1 ; y<=gparams.Lx/2 ; y++){ 
         loop= wilsonloop_non_planar(U, {0, x, y, 0});
         loop+=wilsonloop_non_planar(U, {0, x, 0, y});
@@ -112,6 +113,7 @@ int main(int ac, char* av[]) {
         loop+=wilsonloop_non_planar(U, {0, 0, y, x});
         loop+=wilsonloop_non_planar(U, {0, y, 0, x});
         resultfile << std::setw(14) << std::fixed << loop/U.getVolume()/6.0 << "  " ;
+        
         loop= wilsonloop_non_planar(U, {1, x, y, 0});
         loop+=wilsonloop_non_planar(U, {1, x, 0, y});
         loop+=wilsonloop_non_planar(U, {1, y, x, 0});
@@ -119,10 +121,10 @@ int main(int ac, char* av[]) {
         loop+=wilsonloop_non_planar(U, {1, 0, y, x});
         loop+=wilsonloop_non_planar(U, {1, y, 0, x});
         resultfile << std::setw(14) << std::fixed << loop/U.getVolume()/6.0 << "  " ;
-      }   
+      }
     resultfile << std::endl;
     resultfile.close(); 
-    }  
+    }
   }
   return(0);
 }
--- smearape.hh
+++ smearape.hh
@@ -18,10 +18,14 @@
 
 
 //prescription for smearing taken from https://arxiv.org/abs/hep-lat/0209159
-//not tested for SU2, for U1 lattice stays the same whEn smearing with alpha=1
-template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha){
+//not tested for SU2, for U1 lattice stays the same when smearing with alpha=1
+template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha, bool spacial=false){
   gaugeconfig<Group> Uold = U;
   typedef typename accum_type<Group>::type accum;
+  size_t startmu=0;
+  if(spacial){
+      startmu=1;
+  }
   #ifdef _USE_OMP_
   #pragma omp parallel for
   #endif
@@ -30,7 +34,7 @@ template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha){
       for(size_t x2 = 0; x2 < U.getLy(); x2++) {
         for(size_t x3 = 0; x3 < U.getLz(); x3++) {
           std::vector<size_t> x = {x0, x1, x2, x3};  
-          for(size_t mu = 0; mu < U.getndims(); mu++) {
+          for(size_t mu = startmu; mu < U.getndims(); mu++) {
             accum K;
             get_staples(K, Uold, x, mu);
             K = K*(1-alpha)/2.0;

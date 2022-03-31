5691d4c70004520c2773f6f328085ca5e2221c06
Christiane Gross <chgross.wickede@gmail.com>
2022-03-31 11:22:51 +0200

    Changed normalization factor of staples for APE-smearing, it should now be correct for all dimensions and spatial/not-spatial. Also added check for two dimensions, there it is not possible to smear only the spatial links

--- smearape.hh
+++ smearape.hh
@@ -19,13 +19,20 @@
 
 //prescription for smearing taken from https://arxiv.org/abs/hep-lat/0209159
 //not tested for SU2, for U1 lattice stays the same when smearing with alpha=1
-//~ template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha, const double xi=1.0, bool anisotropic=false, bool spacial=false){
+//norm is the number of staples which are used in the summation
+//if spacial=true, only the staples with mu>=1 are smeared and taken into account for the staples
 template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha, bool spacial=false){
+  if (U.getndims() == 2 && spacial){
+    std::cerr << "Spacial smearing is not possible in two dimensions!" << std::endl;
+    return;
+  }
   gaugeconfig<Group> Uold = U;
   typedef typename accum_type<Group>::type accum;
-  size_t startmu=0;
+  size_t startmu = 0;
+  size_t norm = 2.0*(U.getndims()-1);
   if(spacial){
-      startmu=1;
+      startmu = 1;
+      norm = 2.0*(U.getndims()-2);
   }
   #ifdef _USE_OMP_
   #pragma omp parallel for
@@ -36,10 +43,10 @@ template<class Group> void smearlatticeape(gaugeconfig<Group> &U, double alpha, 
         for(size_t x3 = 0; x3 < U.getLz(); x3++) {
           std::vector<size_t> x = {x0, x1, x2, x3};  
           for(size_t mu = startmu; mu < U.getndims(); mu++) {
-            accum K;
-            //~ get_staples(K, Uold, x, mu, xi, anisotropic, spacial);
+            // K is intialized to (0,0) even if not explicitly specified
+            accum K(0.0, 0.0);
             get_staples(K, Uold, x, mu, 1.0, false, spacial);
-            Group Uprime(Uold(x, mu)*alpha+K*(1-alpha)/2.0);
+            Group Uprime(Uold(x, mu) * alpha + K * (1-alpha) / double(norm));
             U(x, mu) = Uprime;
             U(x, mu).restoreSU();
           }

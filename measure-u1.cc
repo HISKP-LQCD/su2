feba3f2f786d3358f5a76a8a8c21c37ee1ac91c0
Christiane Gross <chgross.wickede@gmail.com>
2022-03-31 11:36:23 +0200

    Changed measurement of Wilson-Loops in U(1): added flag sizeloops: Loops are measured up to a size of lattice extent*sizeloops. Added potentialsmall: also measures non-planar loops for short (<=4) distances. Smearing is applied before measurement of loops and non-planar loops.

--- measure-u1.cc
+++ measure-u1.cc
@@ -19,6 +19,7 @@
 #include<vector>
 #include<random>
 #include<boost/program_options.hpp>
+#include<algorithm>
 
 namespace po = boost::program_options;
 
@@ -38,10 +39,12 @@ int main(int ac, char* av[]) {
   double tau;
   size_t integs;
   bool append;
+  double sizeloops;
   size_t n_apesmear;
   double alpha;
   bool smearspacial;
   bool potential;
+  bool potentialsmall;
 
   cout << "## Measuring Tool for U(1) gauge theory" << endl;
   cout << "## (C) Carsten Urbach <urbach@hiskp.uni-bonn.de> (2017)" << endl;
@@ -60,10 +63,12 @@ int main(int ac, char* av[]) {
     ("exponent", po::value<size_t>(&exponent)->default_value(0), "exponent for rounding")
     ("integrator", po::value<size_t>(&integs)->default_value(0), "itegration scheme to be used: 0=leapfrog, 1=lp_leapfrog, 2=omf4, 3=lp_omf4")
     ("append", po::value<bool>(&append)->default_value(false), "are measurements for potential appended to an existing file, or should it be overwritten?")
+    ("sizeloops", po::value<double>(&sizeloops)->default_value(0.5), "Wilson-Loops are measured up to this fraction of the lattice extent")
     ("napesmears", po::value<size_t>(&n_apesmear)->default_value(0), "number of APE smearings done on the lattice before measurement")
     ("apealpha", po::value<double>(&alpha)->default_value(1.0), "parameter alpha for APE smearings")
     ("spacialsmear", po::value<bool>(&smearspacial)->default_value(false), "should smearing be done only for spacial links?")
     ("potential", po::value<bool>(&potential)->default_value(false), "measure potential")
+    ("potentialsmall", po::value<bool>(&potentialsmall)->default_value(false), "measure all nonplanar potentials with maximum extent 4 in 2+1 D")
     ;
 
   int err = parse_commandline(ac, av, desc, gparams);
@@ -78,6 +83,7 @@ int main(int ac, char* av[]) {
   std::ofstream resultfile;
   char filename[200];
   double loop;
+  size_t maxsizenonplanar = (gparams.Lx < 4) ? gparams.Lx : 4;
   
   if(potential) {
       //~ open file for saving results
@@ -97,9 +103,9 @@ int main(int ac, char* av[]) {
       }
       resultfile.open(filename, std::ios::out);
       resultfile << "##";
-      for (size_t t = 1 ; t <= gparams.Lt/2 ; t++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
-          resultfile << std::setw(5) << "W(x=" << std::setw(2) << x << ", t=" << std::setw(2) << t << ", y=" << std::setw(2) << 0 << ")  " ;
+      for (size_t t = 1 ; t <= gparams.Lt*sizeloops ; t++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
+          resultfile << "W(x=" << x << ",t=" << t << ",y=" << 0 << ")  " ;
           }
       }
       resultfile << "counter";
@@ -113,15 +119,43 @@ int main(int ac, char* av[]) {
       }
       resultfile.open(filename, std::ios::out);
       resultfile << "##";
-      for (size_t y = 1 ; y <= gparams.Ly/2 ; y++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
-          resultfile << std::setw(5) << "W(x=" << std::setw(2) << x << ", t=" << std::setw(2) << 0 << ", y=" << std::setw(2) << y << ")  " ;
+      for (size_t y = 1 ; y <= gparams.Ly*sizeloops ; y++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
+          resultfile << "W(x=" << x << ",t=" << 0 << ",y=" << y << ")  " ;
           }
       }
       resultfile << "counter";
       resultfile << std::endl; 
       resultfile.close();
         
+    }
+  }
+
+  if(potentialsmall) {
+      //~ open file for saving results
+    
+    if(gparams.ndims == 2 || gparams.ndims == 4){
+      std::cout << "Currently not working for dim = 2 and dim = 4, aborting" << std::endl;
+      return 0;
+    }
+    
+    //~ print heads of columns: W(r, t), W(x, y)
+    if(!append && (gparams.ndims == 3)){
+      if(gparams.ndims == 3){
+        sprintf(filename, "result2p1d.u1potential.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%fnonplanar",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
+      }
+      resultfile.open(filename, std::ios::out);
+      resultfile << "##";
+      for (size_t t = 1 ; t <= gparams.Lt*sizeloops ; t++){
+        for (size_t x = 1 ; x <= maxsizenonplanar ; x++){
+          for (size_t y = 1 ; y <= maxsizenonplanar ; y++){
+            resultfile << "W(x=" << x << ",t=" << t << ",y=" << y << ")  " ;
+          }
+        }
+      }
+      resultfile << "counter";
+      resultfile << std::endl; 
+      resultfile.close();        
     }
   }
 
@@ -168,17 +202,18 @@ int main(int ac, char* av[]) {
       gradient_flow(U, os.str(), tmax);
     }
     
-    if(potential) {
-            //smear lattice
+    if(potential || potentialsmall){
+        //smear lattice
       for (size_t smears = 0 ; smears < n_apesmear ; smears +=1){
         smearlatticeape(U, alpha, smearspacial);
       }
+    if(potential) {
       //~ //calculate wilsonloops for potential
       if(gparams.ndims == 4){
         sprintf(filename, "result3p1d.u1potential.rotated.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%ffinedistance",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
         resultfile.open(filename, std::ios::app);
-        for (size_t t = 1 ; t <= gparams.Lt/2 ; t++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
+        for (size_t t = 1 ; t <= gparams.Lt*sizeloops ; t++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
             loop  = wilsonloop_non_planar(U, {t, x, 0, 0});
             resultfile << std::setw(14) << std::scientific << loop/U.getVolume() << "  " ;
           }
@@ -188,8 +223,8 @@ int main(int ac, char* av[]) {
         resultfile.close();
         sprintf(filename, "result3p1d.u1potential.rotated.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%fcoarsedistance",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
         resultfile.open(filename, std::ios::app);
-        for (size_t y = 1 ; y <= gparams.Ly/2 ; y++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
+        for (size_t y = 1 ; y <= gparams.Ly*sizeloops ; y++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
             loop  = wilsonloop_non_planar(U, {0, x, y, 0});
             loop += wilsonloop_non_planar(U, {0, x, 0, y});
             //~ loop += wilsonloop_non_planar(U, {0, y, x});
@@ -203,8 +238,8 @@ int main(int ac, char* av[]) {
       if(gparams.ndims == 3){
         sprintf(filename, "result2p1d.u1potential.rotated.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%ffinedistance",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
         resultfile.open(filename, std::ios::app);
-        for (size_t t = 1 ; t <= gparams.Lt/2 ; t++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
+        for (size_t t = 1 ; t <= gparams.Lt*sizeloops ; t++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
             loop  = wilsonloop_non_planar(U, {t, x, 0});
             resultfile << std::setw(14) << std::scientific << loop/U.getVolume()/1.0 << "  " ;
           }
@@ -214,8 +249,8 @@ int main(int ac, char* av[]) {
         resultfile.close();
         sprintf(filename, "result2p1d.u1potential.rotated.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%fcoarsedistance",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
         resultfile.open(filename, std::ios::app);
-        for (size_t y = 1 ; y <= gparams.Ly/2 ; y++){
-          for (size_t x = 1 ; x <= gparams.Lx/2 ; x++){
+        for (size_t y = 1 ; y <= gparams.Ly*sizeloops ; y++){
+          for (size_t x = 1 ; x <= gparams.Lx*sizeloops ; x++){
             loop  = wilsonloop_non_planar(U, {0, x, y});
             //~ loop += wilsonloop_non_planar(U, {0, y, x});
             resultfile << std::setw(14) << std::scientific << loop/U.getVolume()/1.0 << "  " ;
@@ -226,6 +261,28 @@ int main(int ac, char* av[]) {
         resultfile.close();
       }
     }
+    
+    if(potentialsmall) {
+    if(!append && (gparams.ndims == 3)){
+      if(gparams.ndims == 3){
+        sprintf(filename, "result2p1d.u1potential.Nt%lu.Ns%lu.b%f.xi%f.nape%lu.alpha%fnonplanar",gparams.Lt, gparams.Lx,U.getBeta(), gparams.xi, n_apesmear, alpha);
+      }
+      resultfile.open(filename, std::ios::app);
+      for (size_t t = 1 ; t <= gparams.Lt*sizeloops ; t++){
+        for (size_t x = 1 ; x <= maxsizenonplanar ; x++){
+          for (size_t y = 1 ; y <= maxsizenonplanar ; y++){
+            loop  = wilsonloop_non_planar(U, {t, x, y});
+            resultfile << std::setw(14) << std::scientific << loop/U.getVolume()/1.0 << "  " ;
+          }
+        }
+      }
+      resultfile << i;
+      resultfile << std::endl; 
+      resultfile.close();  
+    }
+  }
+  }
+    
   }
 
   return(0);

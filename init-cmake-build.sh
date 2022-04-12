e7c47182a09d5d81b920f65d87a799933324eaf5
simone-romiti <simone.romiti.1994@gmail.com>
2022-04-12 15:28:47 +0200

    Automatic yaml installation

--- init-cmake-build.sh
+++ init-cmake-build.sh
@@ -2,8 +2,10 @@
 # Copyright © 2017 Martin Ueding <dev@martin-ueding.de>
 # Licensed under the GNU Public License version 3
 
+
 set -e
 set -u
+
 
 if [[ -d build ]]; then
     echo "Directory build exists, this script will do nothing."
@@ -13,7 +15,17 @@ fi
 mkdir -p build/debug
 mkdir -p build/release
 
-YAML_SRC_PATH=/opt/yaml-cpp/ # change it with your path
+# installing yaml-cpp
+rm -rf external/yaml-cpp/build/
+d1=$(pwd -P)
+cd external/yaml-cpp
+mkdir build 
+cd build
+cmake ..
+make -j8
+cd $d1
+
+YAML_SRC_PATH=$d1/external/yaml-cpp/
 
 pushd build/debug
 cmake \

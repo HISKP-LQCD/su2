38051bb4e93abdffa6ad1d1450c001b3749797e8
simone-romiti <simone.romiti.1994@gmail.com>
2022-05-03 08:40:18 +0200

    Simplified the input file. There's no need to specify "begin_" in the name of some nested nodes.

--- parse_input_file.cc
+++ parse_input_file.cc
@@ -73,25 +73,25 @@ namespace input_file_parsing {
 
         parse_geometry(in, pparams);
 
-        if (nd["begin_monomials"]) {
-          if (nd["begin_monomials"]["gauge"]) {
+        if (nd["monomials"]) {
+          if (nd["monomials"]["gauge"]) {
             pparams.include_gauge = true;
-            in.read_verb<double>(pparams.beta, {"begin_monomials", "gauge", "beta"});
+            in.read_verb<double>(pparams.beta, {"monomials", "gauge", "beta"});
           }
           // note: initializing the D*Ddag monomial makes sense only with a valid Dirac
           // operator
-          if (nd["begin_operators"]["staggered"] && nd["begin_monomials"]["staggered_det_DDdag"]) {
+          if (nd["operators"]["staggered"] && nd["monomials"]["staggered_det_DDdag"]) {
             pparams.include_staggered_fermions = true;
 
-            in.read_verb<double>(pparams.m0, {"begin_operators", "staggered", "mass"});
+            in.read_verb<double>(pparams.m0, {"operators", "staggered", "mass"});
 
-            in.read_opt_verb<std::string>(hparams.solver, {"begin_monomials", "staggered_det_DDdag",
+            in.read_opt_verb<std::string>(hparams.solver, {"monomials", "staggered_det_DDdag",
                                            "solver"});
-            in.read_opt_verb<double>(hparams.tolerance_cg, {"begin_monomials", "staggered_det_DDdag",
+            in.read_opt_verb<double>(hparams.tolerance_cg, {"monomials", "staggered_det_DDdag",
                                       "tolerance_cg"});
             in.read_opt_verb<size_t>(hparams.solver_verbosity,
-                                      {"begin_monomials", "staggered_det_DDdag", "solver_verbosity"});
-            in.read_opt_verb<size_t>(hparams.seed_pf, {"begin_monomials", "staggered_det_DDdag",
+                                      {"monomials", "staggered_det_DDdag", "solver_verbosity"});
+            in.read_opt_verb<size_t>(hparams.seed_pf, {"monomials", "staggered_det_DDdag",
                                       "seed_pf"});
           }
         }
@@ -110,11 +110,11 @@ namespace input_file_parsing {
         validate_beta_str_width(hparams.beta_str_width);
 
         // integrator parameters
-        in.read_opt_verb<size_t>(hparams.N_rev, {"begin_integrator", "N_rev"});
-        in.read_opt_verb<size_t>(hparams.n_steps, {"begin_integrator", "n_steps"});
-        in.read_opt_verb<double>(hparams.tau, {"begin_integrator", "tau"});
-        in.read_opt_verb<size_t>(hparams.exponent, {"begin_integrator", "exponent"});
-        in.read_opt_verb<std::string>(hparams.integrator, {"begin_integrator",
+        in.read_opt_verb<size_t>(hparams.N_rev, {"integrator", "N_rev"});
+        in.read_opt_verb<size_t>(hparams.n_steps, {"integrator", "n_steps"});
+        in.read_opt_verb<double>(hparams.tau, {"integrator", "tau"});
+        in.read_opt_verb<size_t>(hparams.exponent, {"integrator", "exponent"});
+        in.read_opt_verb<std::string>(hparams.integrator, {"integrator",
                                        "name"});
 
         in.finalize();
@@ -135,41 +135,41 @@ namespace input_file_parsing {
         parse_geometry(in, pparams);
 
         // beta, xi value from the gauge action
-        in.read_verb<double>(pparams.beta, {"begin_monomials", "gauge", "beta"});
-        in.read_opt_verb<bool>(pparams.anisotropic, {"begin_monomials", "gauge",
+        in.read_verb<double>(pparams.beta, {"monomials", "gauge", "beta"});
+        in.read_opt_verb<bool>(pparams.anisotropic, {"monomials", "gauge",
                                 "anisotropic"});
         if (pparams.anisotropic) {
-          in.read_opt_verb<double>(pparams.xi, {"begin_monomials", "gauge", "xi"});
+          in.read_opt_verb<double>(pparams.xi, {"monomials", "gauge", "xi"});
         }
 
         // measure-u1 parameters
-        in.read_opt_verb<size_t>(mparams.n_meas, {"begin_measurements", "n_meas"});
-        in.read_opt_verb<size_t>(mparams.nstep, {"begin_measurements", "nstep"});
-        in.read_opt_verb<size_t>(mparams.icounter, {"begin_measurements", "icounter"});
-        in.read_opt_verb<size_t>(mparams.seed, {"begin_measurements", "seed"});
-        in.read_opt_verb<bool>(mparams.Wloop, {"begin_measurements", "Wloop"});
+        in.read_opt_verb<size_t>(mparams.n_meas, {"measurements", "n_meas"});
+        in.read_opt_verb<size_t>(mparams.nstep, {"measurements", "nstep"});
+        in.read_opt_verb<size_t>(mparams.icounter, {"measurements", "icounter"});
+        in.read_opt_verb<size_t>(mparams.seed, {"measurements", "seed"});
+        in.read_opt_verb<bool>(mparams.Wloop, {"measurements", "Wloop"});
         // optional parameters for gradient
-        if (nd["begin_measurements"]["gradient"]) {
-          in.read_opt_verb<double>(mparams.tmax, {"begin_measurements", "gradient", "tmax"});
+        if (nd["measurements"]["gradient"]) {
+          in.read_opt_verb<double>(mparams.tmax, {"measurements", "gradient", "tmax"});
         }
         // optional parameters for potentials
-        if (nd["begin_measurements"]["potential"]) {
-          in.read_opt_verb<bool>(mparams.potential, {"begin_measurements", "potential", "potential"});
-          in.read_opt_verb<bool>(mparams.potentialsmall, {"begin_measurements", "potential", "potentialsmall"});
-          in.read_opt_verb<bool>(mparams.append, {"begin_measurements", "potential", "append"});
-          in.read_opt_verb<bool>(mparams.smear_spatial_only, {"begin_measurements", "potential",
+        if (nd["measurements"]["potential"]) {
+          in.read_opt_verb<bool>(mparams.potential, {"measurements", "potential", "potential"});
+          in.read_opt_verb<bool>(mparams.potentialsmall, {"measurements", "potential", "potentialsmall"});
+          in.read_opt_verb<bool>(mparams.append, {"measurements", "potential", "append"});
+          in.read_opt_verb<bool>(mparams.smear_spatial_only, {"measurements", "potential",
                                   "smear_spatial_only"});
-          in.read_opt_verb<size_t>(mparams.n_apesmear, {"begin_measurements", "potential", "n_apesmear"});
-          in.read_opt_verb<double>(mparams.alpha, {"begin_measurements", "potential", "alpha"});
-          in.read_opt_verb<double>(mparams.sizeWloops, {"begin_measurements", "potential", "sizeWloops"});
+          in.read_opt_verb<size_t>(mparams.n_apesmear, {"measurements", "potential", "n_apesmear"});
+          in.read_opt_verb<double>(mparams.alpha, {"measurements", "potential", "alpha"});
+          in.read_opt_verb<double>(mparams.sizeWloops, {"measurements", "potential", "sizeWloops"});
         }
 
-        in.read_opt_verb<std::string>(mparams.confdir, {"begin_measurements", "confdir"});
-        in.read_opt_verb<std::string>(mparams.resdir, {"begin_measurements", "resdir"});
+        in.read_opt_verb<std::string>(mparams.confdir, {"measurements", "confdir"});
+        in.read_opt_verb<std::string>(mparams.resdir, {"measurements", "resdir"});
 
-        in.read_opt_verb<std::string>(mparams.conf_basename, {"begin_measurements", "conf_basename"});
+        in.read_opt_verb<std::string>(mparams.conf_basename, {"measurements", "conf_basename"});
 
-        in.read_opt_verb<size_t>(mparams.beta_str_width, {"begin_measurements", "beta_str_width"});
+        in.read_opt_verb<size_t>(mparams.beta_str_width, {"measurements", "beta_str_width"});
         validate_beta_str_width(mparams.beta_str_width);
 
         in.finalize();
@@ -190,29 +190,29 @@ namespace input_file_parsing {
         parse_geometry(in, pparams);
 
         // beta, xi value from the gauge action
-        in.read_verb<double>(pparams.beta, {"begin_monomials", "gauge", "beta"});
-        in.read_opt_verb<bool>(pparams.anisotropic, {"begin_monomials", "gauge","anisotropic"});
+        in.read_verb<double>(pparams.beta, {"monomials", "gauge", "beta"});
+        in.read_opt_verb<bool>(pparams.anisotropic, {"monomials", "gauge","anisotropic"});
         if (pparams.anisotropic) {
-          in.read_opt_verb<double>(pparams.xi, {"begin_monomials", "gauge", "xi"});
+          in.read_opt_verb<double>(pparams.xi, {"monomials", "gauge", "xi"});
         }
 
         // measure-u1 parameters
-        in.read_opt_verb<size_t>(mcparams.n_meas, {"begin_metropolis", "n_meas"});
-        in.read_opt_verb<size_t>(mcparams.N_save, {"begin_metropolis", "N_save"});
-        in.read_opt_verb<size_t>(mcparams.icounter, {"begin_metropolis", "icounter"});
-        in.read_opt_verb<size_t>(mcparams.seed, {"begin_metropolis", "seed"});
+        in.read_opt_verb<size_t>(mcparams.n_meas, {"metropolis", "n_meas"});
+        in.read_opt_verb<size_t>(mcparams.N_save, {"metropolis", "N_save"});
+        in.read_opt_verb<size_t>(mcparams.icounter, {"metropolis", "icounter"});
+        in.read_opt_verb<size_t>(mcparams.seed, {"metropolis", "seed"});
 
-        in.read_opt_verb<std::string>(mcparams.outdir, {"begin_metropolis", "outdir"});
-        in.read_opt_verb<std::string>(mcparams.conf_basename, {"begin_metropolis", "conf_basename"});
-        in.read_opt_verb<size_t>(mcparams.beta_str_width, {"begin_metropolis", "beta_str_width"});
-        in.read_opt_verb<bool>(mcparams.restart, {"begin_metropolis", "restart"});
+        in.read_opt_verb<std::string>(mcparams.outdir, {"metropolis", "outdir"});
+        in.read_opt_verb<std::string>(mcparams.conf_basename, {"metropolis", "conf_basename"});
+        in.read_opt_verb<size_t>(mcparams.beta_str_width, {"metropolis", "beta_str_width"});
+        in.read_opt_verb<bool>(mcparams.restart, {"metropolis", "restart"});
         if (mcparams.restart) {
-          in.read_opt_verb<std::string>(mcparams.configfilename, {"begin_metropolis", "configfilename"});
+          in.read_opt_verb<std::string>(mcparams.configfilename, {"metropolis", "configfilename"});
         }
 
-        in.read_verb<double>(mcparams.heat, {"begin_metropolis", "heat"});
-        in.read_verb<double>(mcparams.delta, {"begin_metropolis", "delta"});
-        in.read_opt_verb<size_t>(mcparams.N_hit, {"begin_metropolis", "N_hit"});
+        in.read_verb<double>(mcparams.heat, {"metropolis", "heat"});
+        in.read_verb<double>(mcparams.delta, {"metropolis", "delta"});
+        in.read_opt_verb<size_t>(mcparams.N_hit, {"metropolis", "N_hit"});
         
         in.finalize();
 

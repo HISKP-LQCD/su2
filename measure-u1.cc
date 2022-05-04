ae75fc940c2b3ca30e5ffe8a76fb36e649d14ae2
Simone <simone.romiti.1994@gmail.com>
2022-05-04 16:05:16 +0200

    removed useless comment 	modified:   measure-u1.cc

--- measure-u1.cc
+++ measure-u1.cc
@@ -38,25 +38,9 @@ int main(int ac, char* av[]) {
   int err = input_file_parsing::parse_command_line(ac, av, input_file);
   if (err > 0) { return err; }
 
-  // po::options_description desc("Allowed options");
-  // desc.add_options()
-  // ("help,h", "produce this help message")
-  // ("file,f", po::value<std::string>(&input_file)->default_value("NONE"), "yaml input file");
-
-  // po::variables_map vm;
-  // po::store(po::parse_command_line(ac, av, desc), vm);
-  // po::notify(vm);
-
-  // std::cout << "check " << vm.count("help") << "\n";
-
-  // if (vm.count("help") || input_file=="NONE") {
-  //   std::cout << desc << "\n";
-  //   return 0;
-  // }
-
   namespace in_meas = input_file_parsing::u1::measure;
   err = in_meas::parse_input_file(input_file, pparams, mparams);
-  if (err > 0) {  return err; }
+  if (err > 0) { return err; }
   
   boost::filesystem::create_directories(boost::filesystem::absolute(mparams.confdir));
   boost::filesystem::create_directories(boost::filesystem::absolute(mparams.resdir));

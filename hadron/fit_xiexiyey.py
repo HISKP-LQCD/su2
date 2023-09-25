3a085b3e3f20bebf3935dc5737401931a5c3de54
simone-romiti <simone.romiti.1994@gmail.com>
2023-09-25 17:58:01 +0200

    fixed a bug (multivariable case) 	modified:   hadron/fit_xiexiyey.py

--- hadron/fit_xiexiyey.py
+++ hadron/fit_xiexiyey.py
@@ -3,6 +3,15 @@ import numpy as np
 import scipy.optimize as opt
 
 
+def check_data(x, ex, y, ey):
+    b1 = (x.shape[1] == ex.shape[1])
+    b2 = (len(y)==len(ey))
+    b3 = (x.shape[1] == len(y))
+    if not(b1 and b2 and b3):
+        raise ValueError("Error: please check the data passed to the fit function")
+    ####
+####
+
 #' fit a function f(x1,...,xn) with errors on both x and y
 #' N_pts = number of points
 #' x = np.array of shape (n, N_pts)
@@ -12,6 +21,7 @@ import scipy.optimize as opt
 #' for the x_hat I use the mean values of the x (tested to be unstable if far from this)
 #' maxiter = maximum number of iterations for the minimizer
 def fit_xiexiyey(ansatz, x, ex, y, ey, guess, maxiter = 10000, method = "BFGS"):
+    check_data(x=x, ex=ex, y=y, ey=ey) ## check that data is in the correct format
     n_var = x.shape[0] # number of variables
     N_pts = len(y) # number of data points
 
@@ -26,7 +36,7 @@ def fit_xiexiyey(ansatz, x, ex, y, ey, guess, maxiter = 10000, method = "BFGS"):
         ch2_res = 0
         p = p_all[0:np_ansatz]
         p_res = p_all[-(n_var * N_pts):]
-        x_hat = np.array([[p_res[j + n_var*i] for j in range(N_pts)] for i in range(n_var)])
+        x_hat = np.array([[p_res[j + N_pts*i] for j in range(N_pts)] for i in range(n_var)])
         #np.array(p_all[-(n_var * N_pts):]).reshape(n_var, N_pts)
         for i in range(N_pts):
             df1 = y[i] - ansatz(x_hat[:,i], p) # y_i - f_i
@@ -41,7 +51,11 @@ def fit_xiexiyey(ansatz, x, ex, y, ey, guess, maxiter = 10000, method = "BFGS"):
         return ch2_res
     ####
 
-    guess = list(guess)+list(x[0,:]) ## ansatz for x_hat=mean values of x
+    guess = list(guess)
+    ## ansatz for x_hat=mean values of x
+    for iv in range(n_var):
+        guess += list(x[iv,:])
+    ####
     mini = opt.minimize(fun = ch2, x0 = guess, method = method)
 
     ch2_value = ch2(mini.x)

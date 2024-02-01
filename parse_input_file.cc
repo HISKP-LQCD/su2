ef7e0718be8994ee56f26cd7b5d0f405448827b9
simone-romiti <simone.romiti.1994@gmail.com>
2024-02-01 11:24:22 +0100

    remnants of (wrong) temporal-only updates

--- parse_input_file.cc
+++ parse_input_file.cc
@@ -411,7 +411,6 @@ namespace input_file_parsing {
     in.read_opt_verb<size_t>(mcparams.N_save, {"N_save"});
     in.read_opt_verb<size_t>(mcparams.n_heatbath, {"n_heatbath"});
     in.read_opt_verb<size_t>(mcparams.n_overrelax, {"n_overrelax"});
-    in.read_opt_verb<size_t>(mcparams.n_heatbath_temporal, {"n_heatbath_temporal"});
     in.read_opt_verb<size_t>(mcparams.seed, {"seed"});
 
     in.read_opt_verb<std::string>(mcparams.conf_dir, {"conf_dir"});

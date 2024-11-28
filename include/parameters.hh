6d3cf4e8bf17844568e67331b4b1e75eb321fcdc
simone-romiti <simone.romiti.1994@gmail.com>
2024-11-28 17:37:22 +0100

    avoiding lengthy filenames for configurations by default

--- include/parameters.hh
+++ include/parameters.hh
@@ -210,7 +210,7 @@ namespace global_parameters {
     std::string configfilename = ""; // configuration filename used in case of restart
     std::string conf_dir = "./"; // directory where gauge configurations are stored
     std::string conf_basename = "conf"; // root of the output files names
-    bool lengthy_conf_name = true; // add ensemble information in configuration name
+    bool lengthy_conf_name = false; // add ensemble information in configuration name
     size_t beta_str_width = g_beta_str_width; // length of the beta value config filename
 
     size_t N_hit = 10; // N_hit updates are performed on each link during one sweep
@@ -242,7 +242,7 @@ namespace global_parameters {
     std::string configfilename = ""; // configuration filename used in case of restart
     std::string conf_dir = "./"; // directory where gauge configurations are stored
     std::string conf_basename = "conf"; // root of the output files names
-    bool lengthy_conf_name = true; // add ensemble information in configuration name
+    bool lengthy_conf_name = false; // add ensemble information in configuration name
     size_t beta_str_width = g_beta_str_width; // length of the beta value config filename
 
     std::string restart_condition = "hot"; // restart condition: hot, cold, read

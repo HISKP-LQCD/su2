52c6cac918bc7d18d14e22af89bb91bd19d7e6b8
simone-romiti <simone.romiti.1994@gmail.com>
2025-01-06 13:20:59 +0100

    DO NOT MERGE; IN PROGRESS: overrelaxation for SU(3)

--- include/overrelaxation.hh
+++ include/overrelaxation.hh
@@ -31,7 +31,7 @@
  * NOTE: the `engines` arguments is not used for U(1),
  * but is needed to have a consistent overflow with the other SU(N) groups
  */
-template<class URNG>
+template <class URNG>
 void overrelaxation(gaugeconfig<u1> &U,
                     std::vector<URNG> engines,
                     const double &xi,
@@ -65,7 +65,7 @@ void overrelaxation(gaugeconfig<u1> &U,
  *
  * See eq. 7.34 of https://www.worldscientific.com/worldscibooks/10.1142/6065 for SU(N)
  */
-template<class URNG>
+template <class URNG>
 void overrelaxation(gaugeconfig<su2> &U,
                     std::vector<URNG> engines,
                     const double &xi,
@@ -109,12 +109,82 @@ void overrelaxation(gaugeconfig<su2> &U,
   return;
 }
 
-template<class URNG>
+/*
+overrelaxation steps according to
+https://www.sciencedirect.com/science/article/abs/pii/0370269390900322
+
+S is the sum of the staples for a given link U.
+In the rare case of det(S^\dagger S) == 0,
+we overrelax the link with a random group element, like for SU(2):
+see section 4.3.2 of https://link.springer.com/book/10.1007/978-3-642-01850-3
+
+*/
+template <class URNG>
 void overrelaxation(gaugeconfig<su3> &U,
                     std::vector<URNG> engines,
                     const double &xi,
                     const bool &anisotropic) {
-  fatal_error("overrelaxation not implemented for SU(3)!", __func__);
+  typedef typename accum_type<su3>::type accum;
+  std::uniform_int_distribution<int> distribution(1, 3); // note: 3 is included
+
+  const size_t endmu = U.getndims();
+  for (size_t x0_start = 0; x0_start < 2; x0_start++) {
+#pragma omp parallel for
+    for (size_t x0 = x0_start; x0 < U.getLt(); x0 += 2) {
+      size_t thread_num = omp_get_thread_num();
+      for (size_t x1 = 0; x1 < U.getLx(); x1++) {
+        for (size_t x2 = 0; x2 < U.getLy(); x2++) {
+          for (size_t x3 = 0; x3 < U.getLz(); x3++) {
+            const std::vector<size_t> x = {x0, x1, x2, x3};
+            for (size_t mu = 0; mu < endmu; mu++) {
+              accum M; // sum of staples
+              get_staples_MCMC_step(M, U, x, mu, xi, anisotropic);
+              accum MdagM = M.dagger() * M; // M^\dagger M
+              const std::complex<double> det_MdagM = MdagM.det();
+              if (det_MdagM == 0.0) {
+                // random element in the group
+                su3 Vdag;
+                random_element(Vdag, engines[thread_num], 1.0);
+                U(x, mu) = Vdag * U(x, mu) * Vdag;
+              } else {
+                // V and D such that: MdagM = V^\dagger*D*V
+                // NOTE: V^{-1}=V^\dagger because MdagM is hermitean
+                std::vector<accum> Vdag_D = MdagM.diagonalize();
+                su3 Vdag = Vdag_D[0].to_SU3(); // converto to a unitary matrix
+                accum D = Vdag_D[1];
+                su3 V = Vdag.dagger();
+
+                accum H = Vdag * D.pow(+1.0 / 2.0) * V; // (M^\dagger M)^{+1/2}
+                accum H_inv = Vdag * D.pow(-1.0 / 2.0) * V; // (M^\dagger M)^{-1/2}
+                accum O = M * H_inv; // M * (M^\dagger M)^{-1/2}
+
+                det_Odag = O.dagger().det(); // det(O^\dagger)
+
+                // I(\alpha) (in the ref. paper) is proportional to 1_{3 \times 3}
+                // if det(I(\alpha)) = x, then I(\alpha) =  x^{1/3} * 1_{3 \times 3}
+                const double Ialpha_fact = std::pow(det_Odag, 1.0 / 3.0);
+
+                su3 Otilde = (O * Ialpha_fact).to_SU3(); // now it is a unitary matrix
+
+                // building the reflected matrix
+                su3 U_reflected = V * U(x, mu) * Otilde * Vdag;
+
+                const int i_reflection = distribution(engines[thread_num]);
+                
+                // apply one of  the 3 reflections defined in the referencepaper
+                U_reflected.apply_reflection(i_reflection); 
+
+                // overrelaxing the link
+                U(x, mu) = V.dagger() * U_reflected * V * Otilde.dagger();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return;
 
   return;
 }
